440d7571717873f0a5ab19d1e6b6832d062ef78e
chrajeshn <chrajeshn@gmail.com>
2024-04-17 16:09:03 +0530

    first ececution

---
+++ 02-variable.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Rajesh:: Hello Naresh how are you?"
+echo "Naresh:: Hi Rajesh, I am fine. How are you?"
+echo "Rajesh:: I am fine too. How is your work?"
+echo "Naresh:: not bad. I am thinking to upgrade to DevOps"

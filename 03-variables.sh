5fcc0237e24a5f63663e90bb7dc837710696dceb
chrajeshn <chrajeshn@gmail.com>
2024-04-17 16:22:42 +0530

    first ececution

--- 03-variables.sh
+++ 03-variables.sh
@@ -8,6 +8,6 @@ PERSON2=Rahual
 #refering variables
 
 echo "$PERSON1:: Hi $PERSON2, How are you?"
-echo "$PERSON2:: Hello $PERSON2 i am fine. How are you?"
+echo "$PERSON2:: Hello $PERSON1, i am fine. How are you?"
 echo "$PERSON1:: i am fine too. How is your job?"
 echo "$PERSON2:: Not bad. I am thinking to upgrade DevOps"

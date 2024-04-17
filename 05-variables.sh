c17fa3592f0110657634f2be8ae5a773a6a16815
chrajeshn <chrajeshn@gmail.com>
2024-04-17 16:41:57 +0530

    first ececution

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter username::"
+
+read -s USERNAME  #HERE USERNAME IS VARIBALE
+
+echo "Please enter password::"
+
+read -s PASSWORD
+
+echo "user name is: $USERNAME, Password IS: $PASSWORD"
